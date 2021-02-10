9d25e7aced2635ab90f1a123289da3e2e1e8df76
Danny Jenkins <jakeowens@protonmail.com>
2021-02-10 14:31:35 +0100

    Wrote tests for map bounds

--- src/test/java/inf112/skeleton/app/AppTest.java
+++ src/test/java/inf112/skeleton/app/AppTest.java
@@ -7,11 +7,25 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void PlayerIsOnTheMap() {
+        HelloWorld board = new HelloWorld();
+
+        // Test when position X and Y is more than MAP_SIZE
+        board.setPosX(board.MAP_SIZE_X+1);
+        board.setPosY(board.MAP_SIZE_Y+1);
+        System.out.println(board.getPosX());
+        System.out.println(board.getPosY());
+
+        assertTrue("OutOfBoundsException", !board.isOnTheMap());
+
+        // Test when position X and Y is out of bounds of map
+        board.setPosX(-1);
+        board.setPosY(-1);
+        System.out.println(board.getPosX());
+        System.out.println(board.getPosY());
+
+        assertTrue("OutOfBoundsException", !board.isOnTheMap());
     }
 }
