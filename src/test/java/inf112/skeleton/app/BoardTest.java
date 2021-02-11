e4507ebd4682340b808bb424c79e4bb45a64b649
Heming Hanevik <Heming.Hanevik@student.uib.no>
2021-02-11 20:41:47 +0100

    Added tests in BoardTest.java.

--- src/test/java/inf112/skeleton/app/BoardTest.java
+++ src/test/java/inf112/skeleton/app/BoardTest.java
@@ -2,13 +2,30 @@ package inf112.skeleton.app;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 public class BoardTest {
 
-    @Test
-    public void PlayerStaysOnBoardTest() {
-        Board board = new Board();
+    Board board;
 
+    @BeforeEach
+    private void createBoard() {
+        board = new Board();
     }
 
+
+    /** @Test
+    public void PlayerStaysOnBoardTest() {
+
+    }**/
+    @Test
+    public void getMAP_SIZE_XTest() {
+        createBoard();
+        assertEquals(board.MAP_SIZE_X, board.getMAP_SIZE_X());
+    }
+    @Test
+    public void  getMAP_SIZE_YTest() {
+        createBoard();
+        assertEquals(board.MAP_SIZE_Y, board.getMAP_SIZE_Y());
+    }
 }
