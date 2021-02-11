610d3c84c842ec713e776bad74c23f049bdbb32b
Julian-Sander Magnussen <julian-sander.magnussen@student.uib.no>
2021-02-11 20:40:49 +0100

    Moved common entity methods from Flag to Entity

--- src/main/java/inf112/skeleton/app/Entity.java
+++ src/main/java/inf112/skeleton/app/Entity.java
@@ -2,4 +2,24 @@ package inf112.skeleton.app;
 
 public abstract class Entity implements IEntity {
 
+    @Override
+    public int getPosX() {
+        return 0;
+    }
+
+    @Override
+    public int getPosY() {
+        return 0;
+    }
+
+    @Override
+    public void setPosX(int newXValue) {
+
+    }
+
+    @Override
+    public void setPosY(int newYValue) {
+
+    }
+
 }
