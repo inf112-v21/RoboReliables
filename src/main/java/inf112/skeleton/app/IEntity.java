faa1d6865ab3e33c04d0c0b287ff12f296e64120
Julian-Sander Magnussen <julian-sander.magnussen@student.uib.no>
2021-02-12 12:06:38 +0100

    Removed redundant comments in IEntity

--- src/main/java/inf112/skeleton/app/IEntity.java
+++ src/main/java/inf112/skeleton/app/IEntity.java
@@ -2,27 +2,11 @@ package inf112.skeleton.app;
 
 public interface IEntity {
 
-    /**
-     * Get the X coordinate of the entity.
-     * @return the X coordinate
-     */
     int getPosX();
 
-    /**
-     * Get the Y coordinate of the entity.
-     * @return the Y coordinate
-     */
     int getPosY();
 
-    /**
-     * Set the X coordinate of the entity.
-     * @param newXValue the new X coordinate
-     */
     void setPosX(int newXValue);
 
-    /**
-     * Set the Y coordinate of the entity.
-     * @param newYValue the new Y coordinate
-     */
     void setPosY(int newYValue);
 }
