55fc1764a607758633881ccff85af75990489c6d
setsaa <sigurdsets@gmail.com>
2021-02-12 11:28:24 +0100

    Wrote documentation for RoboRally.java

--- src/main/java/inf112/skeleton/app/RoboRally.java
+++ src/main/java/inf112/skeleton/app/RoboRally.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 /**
- *
+ * Sets up gdx to create a new game of RoboRally.
  */
 public class RoboRally {
 
@@ -14,11 +14,6 @@ public class RoboRally {
         cfg.setWindowedMode(500, 500);
 
         Board board = new Board();
-        AbstractPlayer testPlayer = new TestPlayer(0, 0, board);
-
-        int posX = testPlayer.getRobot().getPosX();
-        int posY = testPlayer.getRobot().getPosY();
-
         new Lwjgl3Application(board, cfg);
     }
 }
