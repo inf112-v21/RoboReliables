16b977b0847b0cef22b17e9427f93d5901600673
Anders <jakeowens@protonmail.com>
2021-02-25 17:57:15 +0100

    Added ArrayList for player's hand (and needed imports)

--- src/main/java/inf112/skeleton/app/player/AbstractPlayer.java
+++ src/main/java/inf112/skeleton/app/player/AbstractPlayer.java
@@ -2,21 +2,27 @@ package inf112.skeleton.app.player;
 
 import com.badlogic.gdx.InputAdapter;
 import inf112.skeleton.app.entity.Robot;
+import inf112.skeleton.app.Cards.Card;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * An abstract class for the player.
  */
 public abstract class AbstractPlayer extends InputAdapter implements IAbstractPlayer {
     private final Robot robot;
-    //private Card[] hand;
+
+    /**
+     * ArrayList that contains the cards currently in the player's hand
+     */
+    public List<Card> hand = new ArrayList<>();
 
     public AbstractPlayer(int posX, int posY) {
         robot = new Robot(posX, posY);
         robot.setPosX(posX);
         robot.setPosY(posY);
     }
-
-
 
     public Robot getRobot() {
         return robot;
