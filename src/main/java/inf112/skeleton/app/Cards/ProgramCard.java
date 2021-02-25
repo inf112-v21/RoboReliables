78f189a11f0611b92e5a0f33a06f0b88e6d5dd12
Anders <jakeowens@protonmail.com>
2021-02-25 15:46:31 +0100

    Wrote TODO-comment

--- src/main/java/inf112/skeleton/app/Cards/ProgramCard.java
+++ src/main/java/inf112/skeleton/app/Cards/ProgramCard.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ProgramCard extends Card {
+
+    // TODO start using CardValuesEnum instead of this array
     public final String[] programCardValues = {
         "F1", // forwards 1
         "F2", // forwards 2
@@ -12,7 +14,6 @@ public class ProgramCard extends Card {
         "RL", // rotate left
         "UT", // U-turn (invert rotation)
     };
-
     public final List<String> programCardValuesList = Arrays.asList(programCardValues);
 
     ProgramCard(String cardValue) {
