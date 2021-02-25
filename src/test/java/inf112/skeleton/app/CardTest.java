b83f65420e609f629a7d4ea193411cc2d732d730
Anders <jakeowens@protonmail.com>
2021-02-25 19:25:25 +0100

    Wrote new test and documented existing tests

--- src/test/java/inf112/skeleton/app/CardTest.java
+++ src/test/java/inf112/skeleton/app/CardTest.java
@@ -1,6 +1,6 @@
 package inf112.skeleton.app;
 
-import inf112.skeleton.app.Cards.CardDeck;
+import inf112.skeleton.app.Cards.*;
 import inf112.skeleton.app.player.TestPlayer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -33,6 +33,7 @@ public class CardTest {
 
     /**
      * Check the size of the deck after it has been populated.
+     * Also, print the deck to standard out to show the order of the cards.
      */
     @Test
     public void checkCardDeckSizeAfterPopulated() {
@@ -46,13 +47,19 @@ public class CardTest {
         for (int i = 0; i < 6;  i++) cardDeck.populate("UT");
         assertEquals(84, cardDeck.getSize());
         System.out.println("BEFORE SHUFFLE:");
-        for (int i = 0; i < cardDeck.getSize(); i++) System.out.println(cardDeck.getCardValueAt(i));
+        for (int i = 0; i < cardDeck.getSize(); i++) System.out.print(cardDeck.getCardValueAt(i) + " ");
+        System.out.println();
         cardDeck.shuffle();
         System.out.println("AFTER SHUFFLE:");
-        for (int i = 0; i < cardDeck.getSize(); i++) System.out.println(cardDeck.getCardValueAt(i));
+        for (int i = 0; i < cardDeck.getSize(); i++) System.out.print(cardDeck.getCardValueAt(i) + " ");
+        System.out.println();
 
     }
 
+    /**
+     * Test card dealing.
+     * Once a card is dealt, it should be removed from the deck and placed into the player's hand.
+     */
     @Test
     public void dealCardTest() {
         createCardDeck();
@@ -70,6 +77,26 @@ public class CardTest {
         assertEquals(83, cardDeck.getSize());
         assertEquals(1, player.hand.size());
         assertEquals("F1", player.hand.get(0).cardValue);
+    }
+
+    /**
+     * A card is taken from the deck and placed into the hand of a player.
+     * This test checks to see if this card is in fact the same card, and not just another card with the same value.
+     */
+    @Test
+    public void compareCardsTest() {
+        createCardDeck();
+        for (int i = 0; i < 18; i++) cardDeck.populate("F1");
+        for (int i = 0; i < 12; i++) cardDeck.populate("F2");
+        for (int i = 0; i < 6;  i++) cardDeck.populate("F3");
+        for (int i = 0; i < 6;  i++) cardDeck.populate("B1");
+        for (int i = 0; i < 18; i++) cardDeck.populate("RR");
+        for (int i = 0; i < 18; i++) cardDeck.populate("RL");
+        for (int i = 0; i < 6;  i++) cardDeck.populate("UT");
+
+        Card testCard = cardDeck.getCard(0);
+        cardDeck.dealCard(player);
+        assertEquals(testCard, player.hand.get(0));
     }
 
 }
