36ad6f280557d3532594cf499191dd6e99d2ed13
Anders <jakeowens@protonmail.com>
2021-02-25 15:43:43 +0100

    Added populate() and shuffle() statements to card deck constructor

--- src/main/java/inf112/skeleton/app/Cards/ProgramCardDeck.java
+++ src/main/java/inf112/skeleton/app/Cards/ProgramCardDeck.java
@@ -5,7 +5,28 @@ public class ProgramCardDeck extends CardDeck {
     public CardDeck programCardDeck = new CardDeck();
 
     ProgramCardDeck() {
-        programCardDeck.populate("F1");
+        for (int i = 0; i < 18; i++) {
+            programCardDeck.populate("F1");
+        }
+        for (int i = 0; i < 12; i++) {
+            programCardDeck.populate("F2");
+        }
+        for (int i = 0; i < 6; i++) {
+            programCardDeck.populate("F3");
+        }
+        for (int i = 0; i < 6; i++) {
+            programCardDeck.populate("B1");
+        }
+        for (int i = 0; i < 18; i++) {
+            programCardDeck.populate("RR");
+        }
+        for (int i = 0; i < 18; i++) {
+            programCardDeck.populate("RL");
+        }
+        for (int i = 0; i < 6; i++) {
+            programCardDeck.populate("UT");
+        }
+        shuffle();
     }
 
 
