d1ddb22750b6ccc58a2bb4f45bb8efef65979b60
Anders <jakeowens@protonmail.com>
2021-03-04 16:06:27 +0100

    Wrote comment describing intent of class

--- src/main/java/inf112/skeleton/app/Cards/ProgramCard.java
+++ src/main/java/inf112/skeleton/app/Cards/ProgramCard.java
@@ -3,6 +3,9 @@ package inf112.skeleton.app.Cards;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Class that will be used for interpretation of program cards.
+ */
 public class ProgramCard extends Card {
     ProgramCard(CardValue cardValue) {
         super(cardValue);
