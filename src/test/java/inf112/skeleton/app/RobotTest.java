26de79fdee846c825141af26440175add33eb751
setsaa <sigurdsets@gmail.com>
2021-03-05 15:16:11 +0100

    Implemented tests for RobotTest.

--- src/test/java/inf112/skeleton/app/RobotTest.java
+++ src/test/java/inf112/skeleton/app/RobotTest.java
@@ -1,5 +1,7 @@
 package inf112.skeleton.app;
 
+import inf112.skeleton.app.cards.Card;
+import inf112.skeleton.app.cards.CardValue;
 import inf112.skeleton.app.entity.Robot;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +16,28 @@ public class RobotTest {
     public void makeRobot() {
         board = new Board();
         robot = new Robot(new Location(0,0));
+    }
+
+    @Test
+    public void moveTest() {
+        Card card;
+        card = new Card(CardValue.F1);
+
+        robot.move(card);
+
+        int expectedPositionX = 1;
+        int expectedPositionY = 0;
+        assertEquals(expectedPositionX, robot.getLocation().getX());
+        assertEquals(expectedPositionY, robot.getLocation().getY());
+
+        card = new Card(CardValue.F2);
+
+        robot.move(card);
+
+        expectedPositionX = 3;
+        expectedPositionY = 0;
+        assertEquals(expectedPositionX, robot.getLocation().getX());
+        assertEquals(expectedPositionY, robot.getLocation().getY());
     }
 
     @Test
@@ -43,6 +67,22 @@ public class RobotTest {
         int expectedPositionY = 0;
         assertEquals(robot.getLocation().getX(), expectedPositionX);
         assertEquals(robot.getLocation().getY(), expectedPositionY);
+    }
+
+    @Test
+    public void rotateLeftTest() {
+        robot.rotateLeft(1);
+
+        Direction expectedDirection = Direction.LEFT;
+        assertEquals(expectedDirection, robot.getDirection());
+    }
+
+    @Test
+    public void rotateRightTest() {
+        robot.rotateRight(1);
+
+        Direction expectedDirection = Direction.RIGHT;
+        assertEquals(expectedDirection, robot.getDirection());
     }
 
 
