07a11187896d27f1f2e6a28126d2925d285e0cb6
Julian-Sander Magnussen <julian-sander.magnussen@student.uib.no>
2021-04-20 14:40:50 +0200

    Added ArchiveMarker class

---
+++ src/main/java/inf112/skeleton/app/entity/ArchiveMarker.java
@@ -0,0 +1,10 @@
+package inf112.skeleton.app.entity;
+
+import inf112.skeleton.app.Location;
+
+public class ArchiveMarker extends Entity {
+
+    public ArchiveMarker(Location location) {
+        super(location);
+    }
+}
