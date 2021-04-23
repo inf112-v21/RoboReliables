434ae62542edd339e0d8c444d31f081824fef79b
Anders <jakeowens@protonmail.com>
2021-04-23 13:42:49 +0200

    Added Wall class.

---
+++ src/main/java/inf112/skeleton/app/entity/Wall.java
@@ -0,0 +1,19 @@
+package inf112.skeleton.app.entity;
+
+        import com.badlogic.gdx.graphics.g3d.particles.influencers.DynamicsModifier;
+        import inf112.skeleton.app.Direction;
+        import inf112.skeleton.app.Location;
+
+public class Wall extends Entity {
+
+    private Direction faceDirection;
+
+    public Wall(Location location, Direction faceDirection) {
+        super(location);
+        this.faceDirection = faceDirection;
+    }
+
+    public Direction getFaceDirection() {
+        return faceDirection;
+    }
+}
